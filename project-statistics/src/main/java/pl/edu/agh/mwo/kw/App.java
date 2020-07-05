2188a4bda2ef09bd573526a8f46e10cad7fbd6f2
Kaska <wlodarczyk@student.agh.edu.pl>
2020-07-05 12:30:37 +0200

    Report printing refactoring.

--- project-statistics/src/main/java/pl/edu/agh/mwo/kw/App.java
+++ project-statistics/src/main/java/pl/edu/agh/mwo/kw/App.java
@@ -1,71 +1,97 @@
 package pl.edu.agh.mwo.kw;
 
-import org.apache.commons.cli.CommandLineParser;
-import org.apache.commons.cli.DefaultParser;
-import org.apache.commons.cli.Options;
+import org.apache.commons.cli.*;
 
 import java.nio.file.Files;
+import java.nio.file.InvalidPathException;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.Scanner;
 import java.util.Set;
 
 public class App
 {
-    public static void main( String[] args ) {
-        Options options = new Options();
-        options.addOption("e", "employee", false, "Print employees by working hours in projects")
-                .addOption("m", "month", false, "Print ranking of working hours in months")
-                .addOption("d", "day", false, "Print ten the most busiest days ranking")
-                .addOption("p", "path", true, "Folder path")
-                .addOption("h", "help", false, "Display help information");
+    public static void main( String[] args ){
 
-        //HelpFormatter formatter = new HelpFormatter();
-        //formatter.printHelp("App", options);
+        Options options = generateOptions();
         CommandLineParser parser = new DefaultParser();
 
         boolean guard = true;
         while (guard) {
-//            try {
-//                CommandLine line = parser.parse(options, args);
-//                if (line.hasOption("path")) {
-//                    if (Files.exists(Paths.get(args[0]))) {
-//                        DataLoader loader = new XLSDataLoader();
-//                        loader.loadDataFromFiles(args[0]);
-//                        guard = false;
-//                    }
-//                }
-//            }
-//            catch (ParseException exp) {
-//                System.out.println("Podaj prawidłową ścieżkę do folderu");
-//            }
-            System.out.println("Podaj ścieżkę do pliku");
-            String path = new Scanner(System.in).nextLine();
-            if(Files.exists(Paths.get(path))) {
-                DataLoader loader = new XLSDataLoader();
-                Set<Employee> employees = loader.loadDataFromFiles(path);
-//                employees.forEach(employee -> {
-//                    System.out.println(employee.getName());
-//                    employee.getReports().forEach(
-//                            report -> {
-//                                System.out.println(report.getProject() + " " + report.getDate() + " " + report.getWorkingHours());
-//                            }
-//                    );
-//                });
-
-                RankingGenerator generator1 = new RankingOfMonths(employees);
-                generator1.printRanking();
-                System.out.println();
-                RankingGenerator generator2 = new RankingOfEmployees(employees);
-                generator2.printRanking();
-                System.out.println();
-                RankingGenerator generator3 = new RankingOfWorkingDays(employees);
-                generator3.printRanking();
-                guard = false;
+            System.out.println("Select data folder path:");
+            try {
+                Path path = Paths.get(new Scanner(System.in).nextLine());
+                if (Files.exists(path)) {
+                    DataLoader loader = new XLSDataLoader();
+                    Set<Employee> employees = loader.loadDataFromFiles(path.toString());
+                    if (employees.isEmpty()) {
+                        System.out.println("ERROR: No *.xls files in selected folder. Try again.");
+                        continue;
+                    }
+                    System.out.println("Select ranking type. -h for help.");
+                    CommandLine cmd = generateCommandLine(options, new Scanner(System.in).nextLine().split(" "));
+                    if (cmd.hasOption("help")) {
+                        HelpFormatter formatter = new HelpFormatter();
+                        formatter.printHelp("App", options);
+                        System.out.println();
+                        continue;
+                    }
+                    if (cmd.hasOption("day")) {
+                        System.out.println("=====TEN MOST BUSIEST DAY RANKING=====");
+                        RankingGenerator ranking1 = new RankingOfWorkingDays(employees);
+                        ranking1.printRanking();
+                        System.out.println();
+                    }
+                    if (cmd.hasOption("month")) {
+                        System.out.println("====MOST BUSIEST MONTH RANKING====");
+                        RankingGenerator ranking2 = new RankingOfMonths(employees);
+                        ranking2.printRanking();
+                        System.out.println();
+                    }
+                    if (cmd.hasOption("employee")) {
+                        System.out.println("===MOST BUSIEST EMPLOYEE RANKING===");
+                        RankingGenerator ranking3 = new RankingOfEmployees(employees);
+                        ranking3.printRanking();
+                        System.out.println();
+                    }
+                    guard = false;
+                } else {
+                    System.out.println("ERROR: Wrong path. Try again.");
+                }
             }
-            else{
-                System.out.println("Błędna ścieżka. Spróbuj jeszcze raz.");
+            catch(InvalidPathException ex){
+                System.out.println("ERROR: Wrong path. Try again.");
             }
 
         }
+    }
+
+    private static Options generateOptions(){
+        final Options options = new Options();
+        options.addOption("e", "employee", false, "Print employees by working hours in projects")
+                .addOption("m", "month", false, "Print ranking of working hours in months")
+                .addOption("d", "day", false, "Print ten the most busiest days ranking")
+                .addOption("p", "path", true, "Folder export path")
+                .addOption("h", "help", false, "Display help information");
+        return options;
+    }
+
+    private static CommandLine generateCommandLine(final Options options, final String[] commandLineArguments)
+    {
+        final CommandLineParser cmdLineParser = new DefaultParser();
+        CommandLine commandLine = null;
+        try
+        {
+            commandLine = cmdLineParser.parse(options, commandLineArguments);
+        }
+        catch (ParseException parseException)
+        {
+            System.out.println(
+                    "ERROR: Unable to find command-line arguments "
+                            + Arrays.toString(commandLineArguments));
+            //System.out.println("Select -h for help.");
+        }
+        return commandLine;
     }
 }
