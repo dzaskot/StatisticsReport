d2e732410b974063240eab3ee2d06636e697b122
Kaska <wlodarczyk@student.agh.edu.pl>
2020-07-08 02:10:11 +0200

    Jar file - version with dependencies

--- project-statistics/src/main/java/pl/edu/agh/mwo/kw/App.java
+++ project-statistics/src/main/java/pl/edu/agh/mwo/kw/App.java
@@ -88,73 +88,6 @@ public class App
                 formatter.printHelp("statistics", options);
                 System.exit(1);
             }
-//            System.out.println("Select data folder path:");
-//            try {
-//                Path path = Paths.get(new Scanner(System.in).nextLine());
-//                if (Files.exists(path)) {
-//                    System.out.println("Select ranking type. -h for help.");
-//                    CommandLine cmd = generateCommandLine(options, new Scanner(System.in).nextLine().split(" "));
-//                    if(cmd==null) {
-//                        System.out.println("Incorrect options. Try again.");
-//                        continue;
-//                    }
-//                    List<Ranking> rankings = new LinkedList<>();
-//
-//                    DataLoader loader = new XLSDataLoader();
-//                    Set<Employee> employees = loader.loadDataFromFiles(path.toString());
-//
-//                    if (employees.isEmpty()) {
-//                        System.out.println("ERROR: No *.xls files in selected folder. Try again.");
-//                        continue;
-//                    }
-//
-//                    if (cmd.hasOption("help")) {
-//                        HelpFormatter formatter = new HelpFormatter();
-//                        formatter.printHelp("App", options);
-//                        System.out.println();
-//                        continue;
-//                    }
-//                    if (cmd.hasOption("day")) {
-//                        rankings.add(new RankingOfWorkingDays(employees));
-//                    }
-//                    if (cmd.hasOption("month")) {
-//                        rankings.add(new RankingOfMonths(employees));
-//                    }
-//                    if (cmd.hasOption("employee")) {
-//                        rankings.add(new RankingOfEmployees(employees));
-//                    }
-//
-//                    rankings.forEach(Ranking::printRanking);
-//
-//                    if(cmd.hasOption("export") && !cmd.getOptionValue("x").isEmpty()){
-//                        try{
-//                            Path exportPath = Paths.get(cmd.getOptionValue("x"));
-//                            if(Files.isDirectory(exportPath) && Files.isWritable(exportPath)) {
-//                                HSSFWorkbook workbook = new HSSFWorkbook();
-//                                for(Ranking ranking: rankings){
-//                                    workbook = ranking.exportRanking(workbook);
-//                                }
-//                                writeXLS(workbook, path +"\\Rankings.xls");
-//                            }
-//                            else {
-//                                printError();
-//                                continue;
-//                            }
-//                        }
-//                        catch(InvalidPathException ex){
-//                            printError();
-//                            continue;
-//                        }
-//                    }
-//
-//                    guard = false;
-//                } else {
-//                    printError();
-//                }
-//            }
-//            catch(InvalidPathException ex){
-//                printError();
-//            }
         }
     }
 
@@ -172,24 +105,6 @@ public class App
                 .addOption("d", "day", false, "Print ten the most busiest days ranking");
         return options;
     }
-
-//    private static CommandLine generateCommandLine(final Options options, final String[] commandLineArguments)
-//    {
-//        final CommandLineParser cmdLineParser = new DefaultParser();
-//        CommandLine commandLine = null;
-//        try
-//        {
-//            commandLine = cmdLineParser.parse(options, commandLineArguments);
-//        }
-//        catch (ParseException parseException)
-//        {
-//            System.out.println(
-//                    "ERROR: Unable to find command-line arguments "
-//                            + Arrays.toString(commandLineArguments));
-//            //System.out.println("Select -h for help.");
-//        }
-//        return commandLine;
-//    }
 
     private static void printError(){
         System.out.println("ERROR: Wrong path. Try again.");
